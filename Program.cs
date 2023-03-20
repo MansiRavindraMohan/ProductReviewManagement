78dad359993fae65792a30c1c7707bcf082032cd
MansiRavindraMohan <manasimohan29@gmail.com>
2023-03-20 23:29:24 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management problem");
+            //List<ProductReview> productReviews = new List<ProductReview>();
+            //{
+            //    new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 2, UserId = 2, Rating = 4, Review = "Bad", isLike = false };
+            //    new ProductReview() { ProductId = 3, UserId = 7, Rating = 5, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 4, UserId = 9, Rating = 5, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 5, UserId = 3, Rating = 5, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 6, UserId = 5, Rating = 4, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 7, UserId = 10, Rating = 5, Review = "Bad", isLike = false };
+            //    new ProductReview() { ProductId = 8, UserId = 3, Rating = 4, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 9, UserId = 4, Rating = 5, Review = "Nice", isLike = false };
+            //    new ProductReview() { ProductId = 10, UserId = 2, Rating = 5, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 11, UserId = 3, Rating = 5, Review = "Good", isLike = true };
+            //    new ProductReview() { ProductId = 12, UserId = 6, Rating = 5, Review = "Good", isLike = true };
+            //};
+            //foreach(var list in productReviews)
+            //{
+            //    Console.WriteLine("ProductId : " + list.ProductId + " UserId : " + list.UserId + " Rating : " + list.Rating + " Review : " + list.Review + " isLike : " + list.isLike);
+            //}
+            //Management management = new Management();
+            //management.TopRecordsList(productReviews);
+        }
+    }
+}
